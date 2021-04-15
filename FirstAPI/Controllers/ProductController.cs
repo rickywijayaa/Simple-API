013aab477270ac69d9221436ae693ffa89500f96
Ricky Wijaya <Rickyyluu2@Gmail.com>
2021-04-15 23:10:55 +0700

    Explicit Loading

--- FirstAPI/Controllers/ProductController.cs
+++ FirstAPI/Controllers/ProductController.cs
@@ -3,6 +3,7 @@ using CreateProductModel;
 using Database.MyDbContext;
 using DeleteProductModel;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ProductModel;
 using System;
 using System.Collections.Generic;
@@ -26,7 +27,7 @@ namespace FirstAPI.Controllers
         [HttpGet]
         public IActionResult Index()
         {
-            return StatusCode(200, _context.Products.OrderBy(x => x.Name));
+            return StatusCode(200, _context.Products.Include(x => x.Category).OrderBy(x => x.Name));
         }
 
         [HttpGet("{id}")]
